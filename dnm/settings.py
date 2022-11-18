3b77fce59924686df3acb15d60714144f25f719e
Ismaila Hassan <code.ismailah28@gmail.com>
2022-11-18 11:53:33 +0100

    removing static file caching

--- dnm/settings.py
+++ dnm/settings.py
@@ -141,7 +141,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 else:
     STATIC_ROOT = BASE_DIR.parent.parent / "static"
     STATICFILES_DIRS = [BASE_DIR / "static"]
