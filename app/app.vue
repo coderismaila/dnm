3bb981145e26c2bb53b3dc3f323a94132a29342a
Ismaila Hassan <code.ismailah28@gmail.com>
2024-08-04 03:08:25 +0100

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "DNM",
+  description:
+    "Distribution Network Management (DNM) is a web app to manage distribution networks asset",
+});
+</script>
+
+<template>
+  <NuxtPage />
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</template>
