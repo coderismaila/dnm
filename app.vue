962d41236c4888177086d55d9824c710c9becf76
Ismaila Hassan <code.ismailah28@gmail.com>
2025-05-14 11:42:18 +0100

    chore: build app layout

--- app.vue
+++ app.vue
@@ -1,6 +1,56 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const color = computed(() => colorMode.value === "dark" ? "#09090b" : "#ffffff");
+
+const { theme, radius } = useCustomize();
+
+useHead({
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
+  ],
+  link: [
+    { rel: "icon", href: "/favicon.ico" },
+  ],
+  htmlAttrs: {
+    lang: "en",
+  },
+  bodyAttrs: {
+    class: computed(() => `theme-${theme.value}`),
+    style: computed(() => `--radius: ${radius.value}rem;`),
+  },
+});
+
+const title = "DNM - Distribution Network Management Application";
+const description = "This application helps manage assets of electricity distribution company in Nigeria";
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogUrl: "https://dashboard.dianprata.com",
+  ogImage: "https://nuxt-shadcn-dashboard.vercel.app/social-card.png",
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: "https://nuxt-shadcn-dashboard.vercel.app/social-card.png",
+  twitterCard: "summary_large_image",
+});
+
+const router = useRouter();
+
+defineShortcuts({
+  "G-H": () => router.push("/"),
+  "G-S": () => router.push("/settings"),
+});
+</script>
+
 <template>
-  <div>
+  <NuxtLayout>
     <NuxtRouteAnnouncer />
-    <Button>Click me</Button>
-  </div>
+    <NuxtPage />
+    <Toaster />
+  </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,24 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "shadcn-nuxt"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+  ],
 
   css: ["~/assets/css/main.css"],
+
+  routeRules: {
+    "/settings": { redirect: "/settings/profile" },
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
 
   shadcn: {
 
